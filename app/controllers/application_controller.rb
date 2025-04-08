c1ed84f4a8efcaa6c70d52e4f750b2aeaaafca3c
BigK2A <ilanrodriguez.pro@gmail.com>
2025-04-08 18:25:19 +0200

    Inscription et header via vue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nom, :prenom, :points])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nom, :prenom, :points])
   end
+
+  # Redirection après connexion
+  def after_sign_in_path_for(resource)
+    root_path # Redirige vers l'index (home#index)
+  end
+
+  # Redirection après déconnexion
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end

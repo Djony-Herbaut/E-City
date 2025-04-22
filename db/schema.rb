50c1be64886b84cd5a427cff0de360b2ca44099c
Djony-Herbaut <djonyandrejovinac@gmail.com>
2025-04-22 16:37:35 +0200

    Modification de la vue de l'édition à la création et à la modification des actualités, ajout bouton Retour sur page Notre Histoire

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_10_163654) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_22_130345) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_10_163654) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "objet_connectes", force: :cascade do |t|
+    t.string "titre"
+    t.text "description"
+    t.string "image"
+    t.integer "user_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_objet_connectes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -82,4 +93,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_10_163654) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "actualites", "users"
+  add_foreign_key "objet_connectes", "users"
 end

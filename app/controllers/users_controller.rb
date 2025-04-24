8e9abde5afa9b309af63792df2191c2bc4d4850c
BigK2A <ilanrodriguez.pro@gmail.com>
2025-04-24 14:49:11 +0200

    panel admin dans profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @actualites_utilisateur = @user.actualites.order(created_at: :desc)
+
+    if current_user.admin?
+      @pending_objets = ObjetConnecte.pending
+      @approved_objets = ObjetConnecte.approved
+      @rejected_objets = ObjetConnecte.rejected
+    end
   end
+  
 end

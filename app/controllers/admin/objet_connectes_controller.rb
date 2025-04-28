d17e0e270de3d333103f695ee1d2e68e8d24177b
BigK2A <ilanrodriguez.pro@gmail.com>
2025-04-28 18:48:06 +0200

    ajout de points lors de validation

--- app/controllers/admin/objet_connectes_controller.rb
+++ app/controllers/admin/objet_connectes_controller.rb
@@ -5,11 +5,18 @@ class Admin::ObjetConnectesController < ApplicationController
 
   def update
     @objet = ObjetConnecte.find(params[:id])
+  
     if params[:commit] == "Valider"
       @objet.update(status: 1)
+      @objet.user.increment!(:points, 50) if @objet.user.present?
     elsif params[:commit] == "Rejeter"
       @objet.update(status: 2)
     end
-    redirect_to admin_root_path
-  end
+  
+    respond_to do |format|
+      format.js
+      format.html { head :no_content } 
+    end
+  end  
+
 end
